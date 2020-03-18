8161862b0dda5bf6b3bcc430c65a4bd922e652de
Avery Cole (13ARC8) <avery.cole@queensu.ca>
2020-03-18 17:04:18 -0400

    added controllable work session length

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,38 +1,62 @@
 import * as React from 'react';
-import { Image, StyleSheet, Text, View, Button } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import { RectButton, ScrollView, LongPressGestureHandler } from 'react-native-gesture-handler';
 import * as Progress from 'react-native-progress';
+import TimePicker from "react-native-24h-timepicker";
 
 export default class LinksScreen extends React.Component{
 constructor(props){
     super(props);
-    this.state = {sessionTime: 0};
     this.tick = this.tick.bind(this);
+    this.state = {sessionTime: 0}; //Time elapsed in seconds
+    this.state = {seconds : 0}; //Length of work session in seconds
+    this.state = {progress : 0} //Amount of session elapsed out of 1
+    this.state = {time : ""}
 }
 
-updateBar(){
-  if(this.state.sessionTime < 1){
-    this.setState(prevState => ({
-      sessionTime: prevState.sessionTime + 0.1
-    }));
-  }
-  else{
-    alert("done!");
-    this.setState({sessionTime : 0})
-    clearInterval(this.interval);
-  }
+clearSession(){
+  this.setState({sessionTime : 0,
+    seconds: 0,
+    progress : 0,
+    time : ""
+  });
+  clearInterval(this.interval);
 }
 
-tick() {
-    this.interval = setInterval(() => {
-      this.updateBar()
-    }, 100);  
+tick(){
+  this.interval = setInterval(() => {
+    if(this.state.progress < 1){
+      this.setState(prevState => ({
+        sessionTime: prevState.sessionTime + 1}));
+      this.setState({progress : (this.state.sessionTime/this.state.seconds)})
+    }
+    else{
+      alert("done session");
+      this.clearSession();
+    }
+  }, 10); //set to 1000 for real time scale  
 }
 
 componentWillUnmount() {
   clearInterval(this.interval);
+}
+
+onCancel() {
+  this.clearSession();
+  this.TimePicker.close();
+}
+
+onConfirm(hour, minute) {
+  clearInterval(this.interval);
+  this.setState({ sessionTime : 0,
+   seconds: ((hour * 3600) + (minute * 60)),
+   progress : 0,
+   time: `${hour}:${minute}`
+  });
+  this.TimePicker.close();
+  this.tick();
 }
 
   render(){
@@ -40,9 +64,17 @@ componentWillUnmount() {
         <View style={styles.container}>
         <Image source={require ('../assets/images/robot-dev.png')} />
         <Button title="press to start work session"
-          onPress={ this.tick }
+          onPress={ ()=> this.TimePicker.open() }
         />
-        <Progress.Bar progress = {this.state.sessionTime} width = {200}/>
+        <TimePicker
+          ref={ref => {
+            this.TimePicker = ref;
+          }}
+          onCancel={() => this.onCancel()}
+          onConfirm={(hour, minute) => this.onConfirm(hour, minute)}
+        />
+        <Progress.Bar progress = {this.state.progress} />
+        <Text>Session time : {this.state.time}</Text> 
       </View>
     )
   }
